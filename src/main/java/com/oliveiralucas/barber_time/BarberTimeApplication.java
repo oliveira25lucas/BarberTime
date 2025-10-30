115723cd87b6b0b11b6b3d643e973cd973c391e3
oliveira25lucas <lucas1122oliveira@gmail.com>
2025-10-30 09:54:39 -0300

    feat: inital configuration (docker, application, pom)

---
+++ src/main/java/com/oliveiralucas/barber_time/BarberTimeApplication.java
@@ -0,0 +1,13 @@
+package com.oliveiralucas.barber_time;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberTimeApplication.class, args);
+	}
+
+}
