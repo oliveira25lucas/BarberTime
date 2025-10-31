29a1cca3e5449ce9b696a530ff29408bed0a9769
oliveira25lucas <lucas1122oliveira@gmail.com>
2025-10-31 10:48:56 -0300

    feat: create CustomerService

--- src/main/java/com/oliveiralucas/barber_time/repository/CustomerRepository.java
+++ src/main/java/com/oliveiralucas/barber_time/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.oliveiralucas.barber_time.repository;
 
 import com.oliveiralucas.barber_time.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 }
---
+++ src/main/java/com/oliveiralucas/barber_time/service/CustomerService.java
@@ -0,0 +1,45 @@
+package com.oliveiralucas.barber_time.service;
+
+import com.oliveiralucas.barber_time.model.Customer;
+import com.oliveiralucas.barber_time.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+
+    public Customer create(Customer customer) {
+        customer.setId(null);
+        return customerRepository.save(customer);
+    }
+
+    public Customer findById(Long id) {
+        return customerRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found"));
+    }
+
+    public Customer update(Long id, Customer customer) {
+        Customer existingCustomer = findById(id);
+        existingCustomer.setEmail(customer.getEmail());
+        existingCustomer.setPhone(customer.getPhone());
+        existingCustomer.setAddress(customer.getAddress());
+        return customerRepository.save(existingCustomer);
+    }
+
+    public void delete(Long id) {
+        Customer customer = findById(id);
+        customerRepository.deleteById(id);
+    }
+
+}
